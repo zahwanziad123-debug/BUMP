13d287ecbd86dcd5c9d53fe5f47545253f4624a4
zahwanziad123-debug <zahwanziad.123@gmail.com>
2026-09-18 17:56:06 +0530

    Fix keyframe editor imports and remove obsolete dialog action

--- app/src/main/kotlin/com/bump/visualizer/MainActivity.kt
+++ app/src/main/kotlin/com/bump/visualizer/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Gravity
-import android.widget.EditText
-import android.app.AlertDialog
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
@@ -258,7 +256,6 @@ class MainActivity : Activity() {
             wordTimeline.setWords(visualizer.exportWords())
             wordTimeline.setSelected(visualizer.selectedIndex())
         })
-        keyframes.addView(editButton("EDIT KEYFRAME") { showKeyframeEditor() })
         keyframes.addView(editButton("DELETE KEYFRAME") {
             visualizer.removeSelectedKeyframe()
             wordTimeline.setWords(visualizer.exportWords())
