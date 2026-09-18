da081068ff455ba4f5fc97d527396a7522068b31
zahwanziad123-debug <zahwanziad.123@gmail.com>
2026-09-18 17:29:13 +0530

    Make lyric start time editable

--- app/src/main/kotlin/com/bump/visualizer/LyricWord.kt
+++ app/src/main/kotlin/com/bump/visualizer/LyricWord.kt
@@ -2,7 +2,7 @@ package com.bump.visualizer
 
 data class LyricWord(
     val text: String,
-    val startMs: Long,
+    var startMs: Long,
     var endMs: Long,
     var x: Float = 0f,
     var y: Float = 0f,
